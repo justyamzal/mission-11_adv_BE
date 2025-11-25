10c077fb5e39db449fe031365956f80ccfdc9317
Yadri Amzal <justyamzal@gmail.com>
2025-11-25 17:24:27 +0700

    mod

--- routes/filmSeriesRoutes.js
+++ routes/filmSeriesRoutes.js
@@ -8,16 +8,12 @@ const filmController = require("../controllers/filmSeriesController");
 
 // CREATE Film/Series  (butuh login & JWT)
 router.post("/", auth, filmController.createFilm);
-
 // READ ALL Film/Series
 router.get("/", filmController.getAllFilms);
-
 // READ DETAIL Film/Series
 router.get("/:id", filmController.getFilmById);
-
 // UPDATE Film/Series (butuh login & JWT)
 router.patch("/:id", auth, filmController.updateFilm);
-
 // DELETE Film/Series (butuh login & JWT)
 router.delete("/:id", auth, filmController.deleteFilm);
 
